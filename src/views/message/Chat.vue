334ebadfa77f1278defbb39a19796b5663f343a3
李晨斌 <1015761882@qq.com>
2021-04-19 23:27:45 +0800

    chat first day

--- src/views/message/Chat.vue
+++ src/views/message/Chat.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="chat">
+  <div id="chat" class="chat">
     <nav-bar :title="receiver.nickname?receiver.nickname:receiver.username" class="navbar">
       <template #left>
-        <icon name="arrow-left" size="6vw" @click="$router.back()" />
+        <icon name="arrow-left" size="6vw" @click="back" />
       </template>
     </nav-bar>
+
+    <ul class="message_list">
+      <li v-for="(item,index) in arr" :key="index">
+        <!--发送者-->
+        <div class="message-item-sender" v-if="item.from_id === parseInt(uid)">
+          <div class="sender_item_body">
+            <div class="sender_message_body">{{item.message_body}}</div>
+            <!-- <div class="sender_send_at">{{item.send_at}}</div> -->
+          </div>
+          <div class="avatar">
+            <img :src="sender.avatar_url" />
+          </div>
+        </div>
+        <!--接收者-->
+        <div class="message-item-receiver" v-else>
+          <div class="avatar">
+            <img :src="receiver.avatar_url" />
+          </div>
+          <div class="receiver_item_body">
+            <div class="receiver_message_body">{{item.message_body}}</div>
+            <!-- <div class="receiver_send_at">{{item.send_at}}</div> -->
+          </div>
+        </div>
+      </li>
+    </ul>
     <div class="input">
-      <input placeholder="请输入" />
-      <div class="send_icon">
-        <van-image
-          width="8vw"
-          height="8vw"
-          radius="50%"
-          lazy-load:true
-          show-loading
-          src="@~assets/img/send_icon.svg"
-        />
+      <input v-model="message" placeholder="请输入" />
+      <div class="send_icon" @click="send">
+        <img src="~assets/img/send_icon.svg" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { NavBar, Icon, Image as VanImage } from 'vant'
+import { NavBar, Icon, Toast } from 'vant'
 import io from 'socket.io-client'
 import userRequest from 'network/http'
 const socket = io('http://localhost:8880')
@@ -31,18 +49,79 @@ export default {
   data() {
     return {
       receiver: {},
-      message: ''
+      sender: {},
+      message: '',
+      uid: localStorage.getItem('ID'),
+      arr: []
     }
   },
   components: {
     NavBar,
-    Icon,
-    VanImage
+    Icon
   },
   methods: {
+    //获取用户聊天记录
+    getUserChatRecord() {
+      userRequest
+        .get('/chat/getRecord', {
+          params: { uid: this.uid }
+        })
+        .then(res => {
+          console.log(res.data)
+          if (res.data) {
+            this.arr = res.data
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    //返回并断开连接
+    back() {
+      this.$router.back()
+      this.disconnet()
+      //清空消息数组
+      this.arr = []
+    },
+    join() {
+      socket.emit('join', parseInt(localStorage.getItem('ID')))
+    },
     //发送消息
     send() {
-      socket.emit('message', '你好')
+      if (!this.message) {
+        //不能发送空白内容
+        Toast({
+          type: 'fail',
+          message: '不能发送空消息'
+        })
+        return
+      }
+      const data = {
+        message_body: this.message,
+        to_id: this.receiver.uid,
+        from_id: parseInt(this.uid),
+        send_at: new Date()
+      }
+      this.arr.push(data)
+      socket.emit('send', data)
+      this.message = ''
+      console.log(this.arr)
+    },
+    //接受消息
+    getMessage() {
+      socket.on('getMessage', data => {
+        this.arr.push(data)
+      })
+    },
+    //监听错误
+    onError() {
+      socket.on('Error', error => {
+        console.log(error)
+      })
+    },
+    //断开连接
+    disconnet() {
+      socket.emit('out', parseInt(this.uid))
     },
     //获取聊天对象信息
     getUser(uid) {
@@ -57,10 +136,27 @@ export default {
         .catch(err => {
           console.log(err)
         })
+    },
+    //获取用户自己头像
+    getSenderUser(uid) {
+      userRequest
+        .get('/user/getUserByUid', {
+          params: { uid: uid }
+        })
+        .then(res => {
+          this.sender = res.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   },
   created() {
+    this.join()
+    this.getMessage()
     this.getUser(parseInt(this.$route.params.uid))
+    this.getSenderUser(parseInt(this.uid))
+    this.getUserChatRecord()
   }
 }
 </script>
@@ -71,6 +167,65 @@ export default {
   left: 0;
   right: 0;
   top: 0;
+}
+.chat {
+  height: 100%;
+  overflow: scroll;
+}
+.message_list {
+  margin-top: 12vw;
+  margin-bottom: 12vw;
+}
+.message_list li {
+  margin: 2vw;
+  outline-style: none;
+}
+.avatar {
+  width: 10vw;
+  height: 10vw;
+}
+.avatar img {
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+.message-item-sender {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 2vw;
+}
+.sender_item_body {
+  width: fit-content;
+  margin-right: 2vw;
+}
+.receiver_item_body {
+  margin-left: 2vw;
+}
+.sender_message_body {
+  display: inline-block;
+  position: relative;
+  right: 0;
+  padding: 2vw;
+  margin: 0 2vw 0 0;
+  border-radius: 2vw;
+  background-color: rgb(148, 236, 192);
+}
+.message-item-receiver {
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 2vw;
+}
+.receiver_message_body {
+  display: inline-block;
+  padding: 2vw;
+  margin: 0 0 0 2vw;
+  border-radius: 2vw;
+  background-color: #8fe;
+}
+.receiver_send_at {
+  padding-left: 2vw;
 }
 .input {
   position: fixed;
@@ -85,7 +240,7 @@ export default {
 .input input {
   flex: 1;
   padding: 2vw 10vw 2vw 2vw;
-  margin: 2vw;
+  margin: 2vw 4vw;
   background-color: #eee;
   outline-style: none;
   border-style: none;
@@ -96,6 +251,10 @@ export default {
   right: 0;
   height: 12vw;
   padding: 2vw 0;
-  margin-right: 4vw;
+  margin-right: 6vw;
+}
+.send_icon img {
+  width: 8vw;
+  height: 8vw;
 }
 </style>
